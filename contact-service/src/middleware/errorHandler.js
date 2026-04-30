a9eec308cf01040e0fcfeeff118de0a8c62522cd
Randy Kamradt <randykamradt@gmail.com>
2026-04-30 15:50:13 -0400

    chore: update errorHandler.js via ArchitectAI [2026-04-30]

---
+++ contact-service/src/middleware/errorHandler.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * Central Express error handler.
+ * Must be mounted LAST (after all routes) via app.use(errorHandler).
+ *
+ * @param {Error & { status?: number, errors?: any[] }} err
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ */
+// eslint-disable-next-line no-unused-vars
+function errorHandler(err, req, res, next) {
+  const status = err.status || 500;
+  const message = err.message || 'Internal Server Error';
+  const details = err.errors || [];
+
+  if (status >= 500) {
+    console.error('[ErrorHandler]', err);
+  }
+
+  res.status(status).json({
+    error: message,
+    details,
+  });
+}
+
+module.exports = errorHandler;
